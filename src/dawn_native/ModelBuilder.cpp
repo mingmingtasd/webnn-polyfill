66bab86ce6324402eb487a50d4bc8ae7aaa3c6e2
Ningxin Hu <ningxin.hu@intel.com>
2020-12-17 09:42:26 +0800

    Add nGraph license header for topological_sort

--- src/dawn_native/ModelBuilder.cpp
+++ src/dawn_native/ModelBuilder.cpp
@@ -113,6 +113,24 @@ ModelBase *ModelBuilderBase::CreateModel(NamedOperandsBase const *named_operands
   return model;
 }
 
+// The implementation derives from nGraph topological_sort in
+// https://github.com/openvinotoolkit/openvino/blob/master/ngraph/core/include/ngraph/graph_util.hpp
+//
+//*****************************************************************************
+// Copyright 2017-2020 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//*****************************************************************************
 std::vector<const OperandBase*> ModelBuilderBase::TopologicalSort(
     std::vector<const OperandBase*>& root_nodes) {
   std::stack<const OperandBase*> nodes_to_do;
