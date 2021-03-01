46384f8f3afd00b9b2a7a6f1a48c4d2eda9f751e
fujunwei <junwei.fu@intel.com>
2021-03-01 14:59:24 +0800

    Add TransposeTests

--- src/tests/end2end/TransposeTests.cpp
+++ src/tests/end2end/TransposeTests.cpp
@@ -10,164 +10,100 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "examples/SampleUtils.h"
-#include "gtest/gtest.h"
+#include "src/tests/WebnnTest.h"
 
-class TransposeTests : public testing::Test {};
-
-class Transpose : public utils::WrappedModel {
-  public:
-    Transpose() : mOptions({}) {
+class TransposeTests : public WebnnTest {
+  protected:
+    void CheckTranspose(const std::vector<int32_t>& inputShape,
+                        const std::vector<float>& inputData,
+                        const std::vector<int32_t>& expectedShape,
+                        const std::vector<float>& expectedValue,
+                        const std::vector<int32_t>& permutation = {}) {
+        const webnn::ModelBuilder builder = GetContext().CreateModelBuilder();
+        const webnn::Operand a = utils::BuildInput(builder, "a", inputShape);
+        webnn::TransposeOptions options;
+        options.permutation = permutation.data();
+        options.permutationCount = permutation.size();
+        const webnn::Operand b = builder.Transpose(a, &options);
+        const webnn::Model model = utils::CreateModel(builder, {{"b", b}});
+        const webnn::Compilation compiledModel = utils::AwaitCompile(model);
+        const webnn::Input input = {inputData.data(), inputData.size() * sizeof(float)};
+        const webnn::Result result = utils::AwaitCompute(compiledModel, {{"a", input}}).Get("b");
+        EXPECT_TRUE(utils::CheckShape(result, expectedShape));
+        EXPECT_TRUE(utils::CheckValue(result, expectedValue));
     }
-    webnn::Operand GenerateOutput(webnn::ModelBuilder nn) override {
-        webnn::Operand input = nn.Input("input", InputDesc());
-        if (!mPermutation.empty()) {
-            mOptions.permutation = mPermutation.data();
-            mOptions.permutationCount = mPermutation.size();
-        }
-        return nn.Transpose(input, &mOptions);
-    }
-    void SetPermutation(std::vector<int32_t> permutation) {
-        mPermutation = std::move(permutation);
-    }
-
-  private:
-    webnn::TransposeOptions mOptions;
-    std::vector<int32_t> mPermutation;
 };
 
 TEST_F(TransposeTests, TransposeDefault) {
-    Transpose* transposeDefault = new Transpose();
-    transposeDefault->SetInput(
-        {2, 3, 4}, {0.43376675, 0.264609,   0.26321858, 0.04260185, 0.6862414,  0.26150206,
-                    0.04169406, 0.24857993, 0.14914423, 0.19905873, 0.33851373, 0.74131566,
-                    0.91501445, 0.21852633, 0.02267954, 0.22069663, 0.95799077, 0.17188412,
-                    0.09732241, 0.03296741, 0.04709655, 0.50648814, 0.13075736, 0.82511896});
-    transposeDefault->SetExpectedBuffer({0.43376675, 0.91501445, 0.6862414,  0.95799077, 0.14914423,
-                                         0.04709655, 0.264609,   0.21852633, 0.26150206, 0.17188412,
-                                         0.19905873, 0.50648814, 0.26321858, 0.02267954, 0.04169406,
-                                         0.09732241, 0.33851373, 0.13075736, 0.04260185, 0.22069663,
-                                         0.24857993, 0.03296741, 0.74131566, 0.82511896});
-    transposeDefault->SetExpectedShape({4, 3, 2});
-    EXPECT_TRUE(utils::Test(transposeDefault));
+    const std::vector<int32_t> inputShape = {2, 3, 4};
+    const std::vector<float> inputData = {
+        0.43376675, 0.264609,   0.26321858, 0.04260185, 0.6862414,  0.26150206,
+        0.04169406, 0.24857993, 0.14914423, 0.19905873, 0.33851373, 0.74131566,
+        0.91501445, 0.21852633, 0.02267954, 0.22069663, 0.95799077, 0.17188412,
+        0.09732241, 0.03296741, 0.04709655, 0.50648814, 0.13075736, 0.82511896,
+    };
+    const std::vector<int32_t> expectedShape = {4, 3, 2};
+    const std::vector<float> expectedValue = {
+        0.43376675, 0.91501445, 0.6862414,  0.95799077, 0.14914423, 0.04709655,
+        0.264609,   0.21852633, 0.26150206, 0.17188412, 0.19905873, 0.50648814,
+        0.26321858, 0.02267954, 0.04169406, 0.09732241, 0.33851373, 0.13075736,
+        0.04260185, 0.22069663, 0.24857993, 0.03296741, 0.74131566, 0.82511896,
+    };
+    CheckTranspose(inputShape, inputData, expectedShape, expectedValue);
 }
 
-TEST_F(TransposeTests, TransposePermutations1) {
-    Transpose* transposePermutations1 = new Transpose();
-    transposePermutations1->SetInput(
-        {2, 3, 4}, {
-                       0.7760998,  0.8363521,  0.10145967, 0.00533229, 0.8190919,  0.83241564,
-                       0.39479077, 0.5622921,  0.9306249,  0.00480607, 0.39600816, 0.35415828,
-                       0.43689877, 0.7603583,  0.14368972, 0.11940759, 0.4834097,  0.6982117,
-                       0.7195266,  0.72893023, 0.896649,   0.13060148, 0.07824122, 0.33766487,
-                   });
-    transposePermutations1->SetPermutation({0, 1, 2});
-    transposePermutations1->SetExpectedBuffer({
+TEST_F(TransposeTests, TransposePermutations) {
+    const std::vector<int32_t> inputShape = {2, 3, 4};
+    const std::vector<float> inputData = {
         0.7760998,  0.8363521,  0.10145967, 0.00533229, 0.8190919,  0.83241564,
         0.39479077, 0.5622921,  0.9306249,  0.00480607, 0.39600816, 0.35415828,
         0.43689877, 0.7603583,  0.14368972, 0.11940759, 0.4834097,  0.6982117,
         0.7195266,  0.72893023, 0.896649,   0.13060148, 0.07824122, 0.33766487,
-    });
-    transposePermutations1->SetExpectedShape({2, 3, 4});
-    EXPECT_TRUE(utils::Test(transposePermutations1));
-}
-
-TEST_F(TransposeTests, TransposePermutations2) {
-    Transpose* transposePermutations2 = new Transpose();
-    transposePermutations2->SetInput(
-        {2, 3, 4}, {
-                       0.7760998,  0.8363521,  0.10145967, 0.00533229, 0.8190919,  0.83241564,
-                       0.39479077, 0.5622921,  0.9306249,  0.00480607, 0.39600816, 0.35415828,
-                       0.43689877, 0.7603583,  0.14368972, 0.11940759, 0.4834097,  0.6982117,
-                       0.7195266,  0.72893023, 0.896649,   0.13060148, 0.07824122, 0.33766487,
-                   });
-    transposePermutations2->SetPermutation({0, 2, 1});
-    transposePermutations2->SetExpectedBuffer({
-        0.7760998,  0.8190919,  0.9306249,  0.8363521,  0.83241564, 0.00480607,
-        0.10145967, 0.39479077, 0.39600816, 0.00533229, 0.5622921,  0.35415828,
-        0.43689877, 0.4834097,  0.896649,   0.7603583,  0.6982117,  0.13060148,
-        0.14368972, 0.7195266,  0.07824122, 0.11940759, 0.72893023, 0.33766487,
-    });
-    transposePermutations2->SetExpectedShape({2, 4, 3});
-    EXPECT_TRUE(utils::Test(transposePermutations2));
-}
-
-TEST_F(TransposeTests, TransposePermutations3) {
-    Transpose* transposePermutations3 = new Transpose();
-    transposePermutations3->SetInput(
-        {2, 3, 4}, {
-                       0.7760998,  0.8363521,  0.10145967, 0.00533229, 0.8190919,  0.83241564,
-                       0.39479077, 0.5622921,  0.9306249,  0.00480607, 0.39600816, 0.35415828,
-                       0.43689877, 0.7603583,  0.14368972, 0.11940759, 0.4834097,  0.6982117,
-                       0.7195266,  0.72893023, 0.896649,   0.13060148, 0.07824122, 0.33766487,
-                   });
-    transposePermutations3->SetPermutation({1, 0, 2});
-    transposePermutations3->SetExpectedBuffer({
-        0.7760998,  0.8363521,  0.10145967, 0.00533229, 0.43689877, 0.7603583,
-        0.14368972, 0.11940759, 0.8190919,  0.83241564, 0.39479077, 0.5622921,
-        0.4834097,  0.6982117,  0.7195266,  0.72893023, 0.9306249,  0.00480607,
-        0.39600816, 0.35415828, 0.896649,   0.13060148, 0.07824122, 0.33766487,
-    });
-    transposePermutations3->SetExpectedShape({3, 2, 4});
-    EXPECT_TRUE(utils::Test(transposePermutations3));
-}
-
-TEST_F(TransposeTests, TransposePermutations4) {
-    Transpose* transposePermutations4 = new Transpose();
-    transposePermutations4->SetInput(
-        {2, 3, 4}, {
-                       0.7760998,  0.8363521,  0.10145967, 0.00533229, 0.8190919,  0.83241564,
-                       0.39479077, 0.5622921,  0.9306249,  0.00480607, 0.39600816, 0.35415828,
-                       0.43689877, 0.7603583,  0.14368972, 0.11940759, 0.4834097,  0.6982117,
-                       0.7195266,  0.72893023, 0.896649,   0.13060148, 0.07824122, 0.33766487,
-                   });
-    transposePermutations4->SetPermutation({1, 2, 0});
-    transposePermutations4->SetExpectedBuffer({
-        0.7760998,  0.43689877, 0.8363521,  0.7603583,  0.10145967, 0.14368972,
-        0.00533229, 0.11940759, 0.8190919,  0.4834097,  0.83241564, 0.6982117,
-        0.39479077, 0.7195266,  0.5622921,  0.72893023, 0.9306249,  0.896649,
-        0.00480607, 0.13060148, 0.39600816, 0.07824122, 0.35415828, 0.33766487,
-    });
-    transposePermutations4->SetExpectedShape({3, 4, 2});
-    EXPECT_TRUE(utils::Test(transposePermutations4));
-}
-
-TEST_F(TransposeTests, TransposePermutations5) {
-    Transpose* transposePermutations5 = new Transpose();
-    transposePermutations5->SetInput(
-        {2, 3, 4}, {
-                       0.7760998,  0.8363521,  0.10145967, 0.00533229, 0.8190919,  0.83241564,
-                       0.39479077, 0.5622921,  0.9306249,  0.00480607, 0.39600816, 0.35415828,
-                       0.43689877, 0.7603583,  0.14368972, 0.11940759, 0.4834097,  0.6982117,
-                       0.7195266,  0.72893023, 0.896649,   0.13060148, 0.07824122, 0.33766487,
-                   });
-    transposePermutations5->SetPermutation({2, 0, 1});
-    transposePermutations5->SetExpectedBuffer({
-        0.7760998,  0.8190919,  0.9306249,  0.43689877, 0.4834097,  0.896649,
-        0.8363521,  0.83241564, 0.00480607, 0.7603583,  0.6982117,  0.13060148,
-        0.10145967, 0.39479077, 0.39600816, 0.14368972, 0.7195266,  0.07824122,
-        0.00533229, 0.5622921,  0.35415828, 0.11940759, 0.72893023, 0.33766487,
-    });
-    transposePermutations5->SetExpectedShape({4, 2, 3});
-    EXPECT_TRUE(utils::Test(transposePermutations5));
-}
-
-TEST_F(TransposeTests, TransposePermutations6) {
-    Transpose* transposePermutations6 = new Transpose();
-    transposePermutations6->SetInput(
-        {2, 3, 4}, {
-                       0.7760998,  0.8363521,  0.10145967, 0.00533229, 0.8190919,  0.83241564,
-                       0.39479077, 0.5622921,  0.9306249,  0.00480607, 0.39600816, 0.35415828,
-                       0.43689877, 0.7603583,  0.14368972, 0.11940759, 0.4834097,  0.6982117,
-                       0.7195266,  0.72893023, 0.896649,   0.13060148, 0.07824122, 0.33766487,
-                   });
-    transposePermutations6->SetPermutation({2, 1, 0});
-    transposePermutations6->SetExpectedBuffer({
-        0.7760998,  0.43689877, 0.8190919,  0.4834097,  0.9306249,  0.896649,
-        0.8363521,  0.7603583,  0.83241564, 0.6982117,  0.00480607, 0.13060148,
-        0.10145967, 0.14368972, 0.39479077, 0.7195266,  0.39600816, 0.07824122,
-        0.00533229, 0.11940759, 0.5622921,  0.72893023, 0.35415828, 0.33766487,
-    });
-    transposePermutations6->SetExpectedShape({4, 3, 2});
-    EXPECT_TRUE(utils::Test(transposePermutations6));
+    };
+    const std::vector<std::vector<int32_t>> permutations = {{0, 1, 2}, {0, 2, 1}, {1, 0, 2},
+                                                            {1, 2, 0}, {2, 0, 1}, {2, 1, 0}};
+    const std::vector<std::vector<int32_t>> expectedShapes = {{2, 3, 4}, {2, 4, 3}, {3, 2, 4},
+                                                              {3, 4, 2}, {4, 2, 3}, {4, 3, 2}};
+    const std::vector<std::vector<float>> expectedValues = {
+        {
+            0.7760998,  0.8363521,  0.10145967, 0.00533229, 0.8190919,  0.83241564,
+            0.39479077, 0.5622921,  0.9306249,  0.00480607, 0.39600816, 0.35415828,
+            0.43689877, 0.7603583,  0.14368972, 0.11940759, 0.4834097,  0.6982117,
+            0.7195266,  0.72893023, 0.896649,   0.13060148, 0.07824122, 0.33766487,
+        },
+        {
+            0.7760998,  0.8190919,  0.9306249,  0.8363521,  0.83241564, 0.00480607,
+            0.10145967, 0.39479077, 0.39600816, 0.00533229, 0.5622921,  0.35415828,
+            0.43689877, 0.4834097,  0.896649,   0.7603583,  0.6982117,  0.13060148,
+            0.14368972, 0.7195266,  0.07824122, 0.11940759, 0.72893023, 0.33766487,
+        },
+        {
+            0.7760998,  0.8363521,  0.10145967, 0.00533229, 0.43689877, 0.7603583,
+            0.14368972, 0.11940759, 0.8190919,  0.83241564, 0.39479077, 0.5622921,
+            0.4834097,  0.6982117,  0.7195266,  0.72893023, 0.9306249,  0.00480607,
+            0.39600816, 0.35415828, 0.896649,   0.13060148, 0.07824122, 0.33766487,
+        },
+        {
+            0.7760998,  0.43689877, 0.8363521,  0.7603583,  0.10145967, 0.14368972,
+            0.00533229, 0.11940759, 0.8190919,  0.4834097,  0.83241564, 0.6982117,
+            0.39479077, 0.7195266,  0.5622921,  0.72893023, 0.9306249,  0.896649,
+            0.00480607, 0.13060148, 0.39600816, 0.07824122, 0.35415828, 0.33766487,
+        },
+        {
+            0.7760998,  0.8190919,  0.9306249,  0.43689877, 0.4834097,  0.896649,
+            0.8363521,  0.83241564, 0.00480607, 0.7603583,  0.6982117,  0.13060148,
+            0.10145967, 0.39479077, 0.39600816, 0.14368972, 0.7195266,  0.07824122,
+            0.00533229, 0.5622921,  0.35415828, 0.11940759, 0.72893023, 0.33766487,
+        },
+        {
+            0.7760998,  0.43689877, 0.8190919,  0.4834097,  0.9306249,  0.896649,
+            0.8363521,  0.7603583,  0.83241564, 0.6982117,  0.00480607, 0.13060148,
+            0.10145967, 0.14368972, 0.39479077, 0.7195266,  0.39600816, 0.07824122,
+            0.00533229, 0.11940759, 0.5622921,  0.72893023, 0.35415828, 0.33766487,
+        },
+    };
+    for (size_t i = 0; i < permutations.size(); ++i) {
+        CheckTranspose(inputShape, inputData, expectedShapes[i], expectedValues[i],
+                       permutations[i]);
+    }
 }
