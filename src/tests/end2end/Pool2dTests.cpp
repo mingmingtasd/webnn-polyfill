83c9771191d533afd3a8f12649abcfe7c46b26cb
Ningxin Hu <ningxin.hu@intel.com>
2021-03-01 12:50:21 +0800

    Fix review comments

--- src/tests/end2end/Pool2dTests.cpp
+++ src/tests/end2end/Pool2dTests.cpp
@@ -24,19 +24,19 @@ struct Pool2dOptions {
     webnn::Pool2dOptions mOptions;
 
     const webnn::Pool2dOptions* AsPtr() {
-        if (windowDimensions.size()) {
+        if (!windowDimensions.empty()) {
             mOptions.windowDimensionsCount = windowDimensions.size();
             mOptions.windowDimensions = windowDimensions.data();
         }
-        if (padding.size()) {
+        if (!padding.empty()) {
             mOptions.paddingCount = padding.size();
             mOptions.padding = padding.data();
         }
-        if (strides.size()) {
+        if (!strides.empty()) {
             mOptions.stridesCount = strides.size();
             mOptions.strides = strides.data();
         }
-        if (dilations.data()) {
+        if (!dilations.empty()) {
             mOptions.dilationsCount = dilations.size();
             mOptions.dilations = dilations.data();
         }
@@ -53,7 +53,7 @@ TEST_F(Pool2dTests, MaxPool2d) {
     const webnn::Operand y = builder.MaxPool2d(x, options.AsPtr());
     const webnn::Model model = utils::CreateModel(builder, {{"y", y}});
     const webnn::Compilation compiledModel = utils::AwaitCompile(model);
-    const std::vector<float> dataX({1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16});
+    const std::vector<float> dataX = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
     const webnn::Input inputX = {dataX.data(), dataX.size() * sizeof(float)};
     const webnn::Result result = utils::AwaitCompute(compiledModel, {{"x", inputX}}).Get("y");
     EXPECT_TRUE(utils::CheckShape(result, {1, 1, 2, 2}));
@@ -70,7 +70,7 @@ TEST_F(Pool2dTests, MaxPool2dDilations) {
     const webnn::Operand y = builder.MaxPool2d(x, options.AsPtr());
     const webnn::Model model = utils::CreateModel(builder, {{"y", y}});
     const webnn::Compilation compiledModel = utils::AwaitCompile(model);
-    const std::vector<float> dataX({1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16});
+    const std::vector<float> dataX = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
     const webnn::Input inputX = {dataX.data(), dataX.size() * sizeof(float)};
     const webnn::Result result = utils::AwaitCompute(compiledModel, {{"x", inputX}}).Get("y");
     EXPECT_TRUE(utils::CheckShape(result, {1, 1, 2, 2}));
@@ -87,8 +87,8 @@ TEST_F(Pool2dTests, MaxPool2dPads) {
     const webnn::Operand y = builder.MaxPool2d(x, options.AsPtr());
     const webnn::Model model = utils::CreateModel(builder, {{"y", y}});
     const webnn::Compilation compiledModel = utils::AwaitCompile(model);
-    const std::vector<float> dataX({1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13,
-                                    14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25});
+    const std::vector<float> dataX = {1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13,
+                                      14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
     const webnn::Input inputX = {dataX.data(), dataX.size() * sizeof(float)};
     const webnn::Result result = utils::AwaitCompute(compiledModel, {{"x", inputX}}).Get("y");
     EXPECT_TRUE(utils::CheckShape(result, {1, 1, 5, 5}));
@@ -106,8 +106,8 @@ TEST_F(Pool2dTests, MaxPool2dStrides) {
     const webnn::Operand y = builder.MaxPool2d(x, options.AsPtr());
     const webnn::Model model = utils::CreateModel(builder, {{"y", y}});
     const webnn::Compilation compiledModel = utils::AwaitCompile(model);
-    const std::vector<float> dataX({1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13,
-                                    14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25});
+    const std::vector<float> dataX = {1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13,
+                                      14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
     const webnn::Input inputX = {dataX.data(), dataX.size() * sizeof(float)};
     const webnn::Result result = utils::AwaitCompute(compiledModel, {{"x", inputX}}).Get("y");
     EXPECT_TRUE(utils::CheckShape(result, {1, 1, 2, 2}));
@@ -123,7 +123,7 @@ TEST_F(Pool2dTests, AveragePool2d) {
     const webnn::Operand y = builder.AveragePool2d(x, options.AsPtr());
     const webnn::Model model = utils::CreateModel(builder, {{"y", y}});
     const webnn::Compilation compiledModel = utils::AwaitCompile(model);
-    const std::vector<float> dataX({1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16});
+    const std::vector<float> dataX = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
     const webnn::Input inputX = {dataX.data(), dataX.size() * sizeof(float)};
     const webnn::Result result = utils::AwaitCompute(compiledModel, {{"x", inputX}}).Get("y");
     EXPECT_TRUE(utils::CheckShape(result, {1, 1, 2, 2}));
@@ -140,8 +140,8 @@ TEST_F(Pool2dTests, AveragePool2dPads) {
     const webnn::Operand y = builder.AveragePool2d(x, options.AsPtr());
     const webnn::Model model = utils::CreateModel(builder, {{"y", y}});
     const webnn::Compilation compiledModel = utils::AwaitCompile(model);
-    const std::vector<float> dataX({1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13,
-                                    14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25});
+    const std::vector<float> dataX = {1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13,
+                                      14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
     const webnn::Input inputX = {dataX.data(), dataX.size() * sizeof(float)};
     const webnn::Result result = utils::AwaitCompute(compiledModel, {{"x", inputX}}).Get("y");
     EXPECT_TRUE(utils::CheckShape(result, {1, 1, 5, 5}));
@@ -160,8 +160,8 @@ TEST_F(Pool2dTests, AveragePool2dStrides) {
     const webnn::Operand y = builder.AveragePool2d(x, options.AsPtr());
     const webnn::Model model = utils::CreateModel(builder, {{"y", y}});
     const webnn::Compilation compiledModel = utils::AwaitCompile(model);
-    const std::vector<float> dataX({1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13,
-                                    14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25});
+    const std::vector<float> dataX = {1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13,
+                                      14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
     const webnn::Input inputX = {dataX.data(), dataX.size() * sizeof(float)};
     const webnn::Result result = utils::AwaitCompute(compiledModel, {{"x", inputX}}).Get("y");
     EXPECT_TRUE(utils::CheckShape(result, {1, 1, 2, 2}));
@@ -175,18 +175,18 @@ TEST_F(Pool2dTests, GlobalAveragePool2d) {
     const webnn::Operand y = builder.AveragePool2d(x);
     const webnn::Model model = utils::CreateModel(builder, {{"y", y}});
     const webnn::Compilation compiledModel = utils::AwaitCompile(model);
-    const std::vector<float> dataX(
-        {-1.1289884,  0.34016284,  0.497431,    2.1915932,   0.42038894,  -0.18261199, -0.15769927,
-         -0.26465914, 0.03877424,  0.39492005,  -0.33410737, 0.74918455,  -1.3542547,  -0.0222946,
-         0.7094626,   -0.09399617, 0.790736,    -0.75826526, 0.27656242,  0.46543223,  -1.2342638,
-         1.1549494,   0.24823844,  0.75670505,  -1.7108902,  -1.4767597,  -1.4969662,  -0.31936142,
-         0.5327554,   -0.06070877, 0.31212643,  2.2274113,   1.2775147,   0.59886885,  -1.5765078,
-         0.18522178,  0.22655599,  0.88869494,  0.38609484,  -0.05860576, -0.72732115, -0.0046324,
-         -1.3593693,  -0.6295078,  1.384531,    0.06825881,  0.19907428,  0.20298219,  -0.8399954,
-         1.3583295,   0.02117888,  -1.0636739,  -0.30460566, -0.92678875, -0.09120782, -0.88333017,
-         -0.9641269,  0.6065926,   -0.5830042,  -0.81138134, 1.3569402,   1.2891295,   0.2508177,
-         0.20211531,  0.8832168,   -0.19886094, -0.61088,    0.682026,    -0.5253442,  1.5022339,
-         1.0256356,   1.0642492,   -0.4169051,  -0.8740329,  1.1494869});
+    const std::vector<float> dataX = {
+        -1.1289884,  0.34016284,  0.497431,    2.1915932,   0.42038894,  -0.18261199, -0.15769927,
+        -0.26465914, 0.03877424,  0.39492005,  -0.33410737, 0.74918455,  -1.3542547,  -0.0222946,
+        0.7094626,   -0.09399617, 0.790736,    -0.75826526, 0.27656242,  0.46543223,  -1.2342638,
+        1.1549494,   0.24823844,  0.75670505,  -1.7108902,  -1.4767597,  -1.4969662,  -0.31936142,
+        0.5327554,   -0.06070877, 0.31212643,  2.2274113,   1.2775147,   0.59886885,  -1.5765078,
+        0.18522178,  0.22655599,  0.88869494,  0.38609484,  -0.05860576, -0.72732115, -0.0046324,
+        -1.3593693,  -0.6295078,  1.384531,    0.06825881,  0.19907428,  0.20298219,  -0.8399954,
+        1.3583295,   0.02117888,  -1.0636739,  -0.30460566, -0.92678875, -0.09120782, -0.88333017,
+        -0.9641269,  0.6065926,   -0.5830042,  -0.81138134, 1.3569402,   1.2891295,   0.2508177,
+        0.20211531,  0.8832168,   -0.19886094, -0.61088,    0.682026,    -0.5253442,  1.5022339,
+        1.0256356,   1.0642492,   -0.4169051,  -0.8740329,  1.1494869};
     const webnn::Input inputX = {dataX.data(), dataX.size() * sizeof(float)};
     const webnn::Result result = utils::AwaitCompute(compiledModel, {{"x", inputX}}).Get("y");
     EXPECT_TRUE(utils::CheckShape(result, {1, 3, 1, 1}));
